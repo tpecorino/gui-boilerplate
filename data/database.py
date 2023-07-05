1eb94ded6a96b5c7361b98661e6e701b82edaacb
tpecorino <webcodedevelop@protonmail.com>
2023-07-04 21:47:23 -0400

    graph generator

--- data/database.py
+++ data/database.py
@@ -4,48 +4,67 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 SQLALCHEMY_DATABASE_URL = "sqlite:///./data/db.sqlite"
-TABLE_NAME = "characters"
+TABLE_NAME = "graphs"
 
 
-class Character(Base):
+class Graph(Base):
     __tablename__ = TABLE_NAME
 
     id = Column("id", Integer, primary_key=True, autoincrement=True,
                 nullable=False, unique=True)
-    name = Column("name", String(25))
+    title = Column("title", String(25))
+    x_axis_label = Column("x_axis_label", String(25))
+    y_axis_label = Column("y_axis_label", String(25))
+    x_axis_values = Column("x_axis_values", String(250))
+    y_axis_values = Column("y_axis_values", String(250))
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title, x_axis_label, y_axis_label, x_axis_values, y_axis_values):
+        self.title = title
+        self.x_axis_label = x_axis_label
+        self.y_axis_label = y_axis_label
+        self.x_axis_values = x_axis_values
+        self.y_axis_values = y_axis_values
 
     def __repr__(self):
-        return f"Todo(id={self.id!r}, name={self.name!r},"
+        return f"Graph" \
+               f"(id={self.id!r}, " \
+               f"title={self.title!r}, " \
+               f"x_axis_label={self.x_axis_label!r}, " \
+               f"y_axis_label={self.y_axis_label!r}," \
+               f"x_axis_label={self.x_axis_values!r}," \
+               f"y_axis_label={self.y_axis_values!r},"
 
 
-def seed_database(db):
-    character_1 = Character("Picard")
-    character_2 = Character("Riker")
-    character_3 = Character("Data")
-    character_4 = Character("Worf")
-
-    db.add(character_1)
-    db.add(character_2)
-    db.add(character_3)
-    db.add(character_4)
-    db.commit()
+# def seed_database(db):
+#     character_1 = Graph("Graph 1", 'amount', 'months')
+#
+#     db.add(character_1)
+#     db.commit()
 
 
-def fetch_characters(db):
-    return db.query(Character).all()
+def fetch_graphs(db):
+    return db.query(Graph).all()
 
 
 def fetch_entity_by_id(db, pk):
-    return db.query(Character).filter(Character.id == pk).first()
+    return db.query(Graph).filter(Graph.id == pk).first()
 
 
-def update_character(db, pk, updated_character):
-    db.query(Character).filter(Character.id == pk).update({
-        "name": updated_character.name
-    })
+def update_graph(db, pk, updated_character):
+    db.query(Graph).filter(Graph.id == pk).update(updated_character)
+    db.commit()
+
+
+def save_graph(db, graph):
+    graph = Graph(graph["title"], graph["x_axis_label"], graph["y_axis_label"], graph["x_axis_values"],
+                  graph["y_axis_values"])
+    db.add(graph)
+    db.commit()
+    return graph
+
+
+def delete_graph(db, graph_id):
+    db.query(Graph).filter(Graph.id == graph_id).delete()
     db.commit()
 
 
@@ -65,10 +84,10 @@ if __name__ == "__main__":
     session = Session()
 
     table_exists = inspect(engine).has_table(TABLE_NAME)
-    character_data = fetch_characters(session)
+    graph_data = fetch_graphs(session)
 
-    if not character_data:
-        seed_database(session)
+    # if not graph_data:
+    #     seed_database(session)
 
-    for Character in character_data:
-        print(f"Character: {Character.name}")
+    for Graph in graph_data:
+        print(f"Graph: {Graph.name}")
