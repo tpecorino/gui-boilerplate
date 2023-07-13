49935fe3e8c36fdbfc6fdf932307215de77668c9
tpecorino <webcodedevelop@protonmail.com>
2023-07-12 20:40:31 -0400

    add clear button

--- util/graph_generator.py
+++ util/graph_generator.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 
 def generate_line_graph(x_label, y_label, x_values, y_values):
-    plt.plot(x_values, y_values, x_values, 'o-r')
+    plt.plot(x_values, y_values, 'o-r')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.grid()
     plt.show()
